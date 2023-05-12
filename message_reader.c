c6488903e43509d25e7b672600111205faa9ba50
Guy Reuveni <guyreuveni98@gmail.com>
2023-05-12 18:29:12 +0300

    wrote sender and reader without err handling

--- message_reader.c
+++ message_reader.c
@@ -1,1 +1,37 @@
+#include "message_slot.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    /*TODO: add error handling in all functions*/
+    int fd, msg_length;
+    char buffer[BUF_LEN];
+
+
+    if (argc != 3){
+        /*TODO: handle error*/
+        exit(1);
+    }
+
+    /*TODO: make sure you need to open read only*/
+    fd = open(argv[1], O_RDONLY);
+
+    ioctl(fd, MSG_SLOT_CHANNEL, atoi(argv[2]));
+
+    msg_length = read(fd, buffer, BUF_LEN);
+
+    close(fd);
+
+    write(STDOUT_FILENO, buffer, msg_length);
+
+
+    /*TODO: it says in the instructions to exit the program
+    with exit value 0. does returning 0 is good?*/
+    return 0;
+}
+
 
--- message_sender.c
+++ message_sender.c
@@ -1,1 +1,38 @@
+#include "message_slot.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    /*TODO: add error handling in all functions*/
+    int fd;
+    char* msg;
+
+
+    if (argc != 4){
+        /*TODO: handle error*/
+        exit(1);
+    }
+
+    /*TODO: make sure you need to open write only*/
+    fd = open(argv[1], O_WRONLY);
+
+    ioctl(fd, MSG_SLOT_CHANNEL, atoi(argv[2]));
+
+    msg = argv[3];
+
+    write(fd, msg, strlen(msg));
+
+    close(fd);
+
+    /*TODO: it says in the instructions to exit the program
+    with exit value 0. does returning 0 is good?*/
+    return 0;
+}
+
+
 
